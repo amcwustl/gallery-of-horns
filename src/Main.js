4d98a38f4908576052987325c22bccdbc7831dd9
Aaron Clark <amclarkwustl@gmail.com>
2023-06-05 15:22:04 -0700

    initial setup

---
+++ src/Main.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import HornedBeast from './HornedBeast';
+
+class Main extends React.Component {
+  render(){
+    return (
+      <>
+      <HornedBeast image_url="" title ="" description = ""/>
+      </>
+    )
+  }
+}
+
+export default Main;
